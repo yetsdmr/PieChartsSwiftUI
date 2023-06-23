47bf0a7535dd2b919380d41a86e47c8791e125a1
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-06-23 17:22:27 +0300

    Initial Commit

---
+++ PieChartsSwiftUI/PieChartsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartsSwiftUIApp.swift
+//  PieChartsSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PieChartsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
